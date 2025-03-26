827c5af5ab16dcdb3f8db10f0bfea7f30ed59453
KI HYUN YOON <116802387+spin54@users.noreply.github.com>
2025-03-26 16:51:14 +0900

    b_10998

---
+++ 입출력과 사칙연산/b_10998.js
@@ -0,0 +1,9 @@
+function solve() {
+    const input = require('fs').readFileSync('/dev/stdin').toString().split(' ');
+    const A = parseInt(input[0]);
+    const B = parseInt(input[1]);
+
+    console.log(A * B);
+}
+
+solve();
